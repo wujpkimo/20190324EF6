39edb46924e9c3491e3eba0d26268516339f7bc7
Jason Wu(Aero 15) <wujpkimo@gmail.com>
2019-03-24 10:35:32 +0800

    20190324 Jason make some exercise

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApp1.Models;
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var db = new ContosoUniversity190324Entities();
+
+            // 顯示程式碼
+            db.Database.Log = (msg) => Console.WriteLine(msg);
+
+            // Contains用法(相當於like % %)
+            var c = db.Courses.Where(p => p.Title.Contains("Git")).ToList();
+
+            foreach (var item in c)
+            {
+                Console.WriteLine(item.Title);
+            }
         }
     }
 }
