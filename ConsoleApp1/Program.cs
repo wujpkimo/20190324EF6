fbb193bef39e04b932cd2d154e75654381fa67c9
Jason Wu(Aero 15) <wujpkimo@gmail.com>
2019-03-24 10:55:45 +0800

    use Lambda style to code LINQ and limite data by select  and to exercise orderby method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,6 +19,26 @@ namespace ConsoleApp1
             // Contains用法(相當於like % %)
             var c = db.Courses.Where(p => p.Title.Contains("Git")).ToList();
 
+            //var c = db.Courses.Where(p => p.Title.StartsWith("Git")).ToList();
+            //var c = db.Courses.Where(p => p.Title.EndsWith("Git")).ToList();
+
+            // 只選擇單一欄位跑SQL
+            //var c = from p in db.Courses
+            //        where p.Title.StartsWith("Git")
+            //        select new
+            //        {
+            //            p.Title
+            //        };
+
+            // 只選擇單一欄位跑SQL+排序
+            //var c = from p in db.Courses
+            //        where p.Title.StartsWith("Git")
+            //        orderby p.Credits descending
+            //        select new
+            //        {
+            //            p.Title
+            //        };
+
             foreach (var item in c)
             {
                 Console.WriteLine(item.Title);
